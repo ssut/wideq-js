f984901615d0c87cab1fb142a886e58759169c4c
Andrey Klimov <aklimov@gmcs.ru>
2020-01-20 16:01:18 +0300

    Fix for comments

--- lib/utils.ts
+++ lib/utils.ts
@@ -14,24 +14,37 @@ export function asEnum<E extends Record<keyof E, string | number>, K extends str
   return k as any; // assertion
 };
 
+/**
+ * Looks up an enum value for the provided attr.
+ * @param attr The attribute to lookup in the enum.
+ * @param data The JSON data from the API.
+ * @param device A sub-class instance of a Device.
+ * @returns The enum value.
+ */
 export function lookupEnum(attr: string, data: any, device: Device) {
-  /**
-   * Looks up an enum value for the provided attr.
-   * @param attr: The attribute to lookup in the enum.
-   * @param data: The JSON data from the API.
-   * @param device: A sub-class instance of a Device.
-   * @returns: The enum value.
-   */
   return device.model.enumName(attr, data[attr]);
 }
 
-export function lookupReference(attr: string, data: any, device: Device) {
 /**
  * Look up a reference value for the provided attribute.
- * @param attr: The attribute to find the value for.
- * @param data: The JSON data from the API.
- * @param device: A sub-class instance of a Device.
- * @returns: The looked up value.
+ * @param attr The attribute to find the value for.
+ * @param data The JSON data from the API.
+ * @param device A sub-class instance of a Device.
+ * @returns The looked up value.
  */
+export function lookupReference(attr: string, data: any, device: Device) {
   return device.model.referenceName(attr, data[attr]);
+}
+
+/**
+ * Looks up an enum value for the provided attr.
+ * @param attr The attribute to lookup in the enum.
+ * @param data The JSON data from the API.
+ * @param device A sub-class instance of a Device.
+ * @returns The enum value.
+ */
+export function lookupEnumLang(attr: string, data: any, device: Device) {
+  const value = device.model.enumName(attr, data[attr]);
+  const lang = value && (device.langPackProduct.enumName(value) || device.langPackModel.enumName(value));
+  return String(lang);
 }
